8ca8198c771bccb3a87a7426f8cbc769ef9f7b4e
wyatt.lamberth <wyatt.lamberth@skylarkwireless.com>
2026-01-12 10:21:38 -0600

    resolved lazy lock conflict

---
+++ nvim/lua/plugins/minuet.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    "milanglacier/minuet-ai.nvim",
+    config = function()
+      require("minuet").setup({
+        provider = "openai_fim_compatible",
+        throttle = 200,
+        debounce = 150,
+        request_timeout = 10,
+        context_window = 2048,
+        n_completions = 1,
+        provider_options = {
+          openai_fim_compatible = {
+            api_key = "TERM",
+            name = "Ollama",
+            end_point = "http://localhost:11434/v1/completions",
+            model = "qwen2.5-coder:7b",
+            optional = {
+              max_tokens = 128,
+              top_p = 0.9,
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    opts = {
+      keymap = {
+        ["<A-y>"] = {
+          function(cmp)
+            cmp.show({ providers = { "minuet" } })
+          end,
+        },
+      },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer", "minuet" },
+        providers = {
+          minuet = {
+            name = "minuet",
+            module = "minuet.blink",
+            score_offset = 8,
+          },
+        },
+      },
+    },
+  },
+}
